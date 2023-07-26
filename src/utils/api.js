1e70b32efb5a441a175ceb5c6b630376f767f1c0
CMLTTN <cemaldim@gmail.com>
2023-07-26 14:57:06 +0300

    api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
  import axios from "axios";
 
   const BASE_URL = "https://api.themoviedb.org/3";
-  const TMDB_TOKEN =  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2Q3Y2M0ODJlMDIyNGJkODNlZTdjMDEyYTRhNGZmYSIsInN1YiI6IjY0MGIyY2QxM2E0YTEyMDBlZTU0MWQ2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XJj6NyBCAFjp6b-RTyEkqD7So-UoLJqJ6OZ3_3c8ZI8"
+  const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
   const headers = {
     Authorization: `Bearer ${TMDB_TOKEN}`,
